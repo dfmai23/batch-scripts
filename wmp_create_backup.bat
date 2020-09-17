b642b00d1984315afb238882ed596f8b08a7a74b
DFM <dfmai@ucdavis.edu>
2020-09-17 01:34:00 -0600

    wmp

---
+++ wmp_create_backup.bat
@@ -0,0 +1,8 @@
+copy /b "D:\Windows\AppData\Local\Microsoft\Media Player\CurrentDatabase_400.wmdb" "CurrentDatabase_400.wmdb"
+copy /b "CurrentDatabase_400.wmdb" "CurrentDatabase_400 - %date%.wmdb"
+
+copy /b "D:\Windows\AppData\Local\Microsoft\Media Player\LocalMLS_0.wmdb" "LocalMLS_0.wmdb"
+copy /b "LocalMLS_0.wmdb" "LocalMLS_0 - %date%.wmdb"
+
+copy /b "D:\Windows\AppData\Local\Microsoft\Media Player\LocalMLS_2.wmdb" "LocalMLS_2.wmdb"
+copy /b "LocalMLS_2.wmdb" "LocalMLS_2 - %date%.wmdb"
